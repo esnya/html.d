8dcaf9665fa1a104a72b49f1f8d3847b6669315c
ukatama <lookout25143@gmail.com>
2014-12-06 02:05:56 +0900

    Add HTMLElement.text

--- html.d
+++ html.d
@@ -11,23 +11,36 @@ class HTMLElement {
     /// 
     string[string] attributes;
 
-    /// tagとattributesからエレメントを生成
+    ///
+    string text;
+
+    /// エレメントを生成
     this(string tag, string[string] attributes = null) {
         this.tag = tag;
         this.attributes = attributes;
     }
-}
+    /// ditto
+    this(string tag, string text, string[string] attributes = null) {
+        this(tag, attributes);
+        this.text = text;
+    }
+    ///
+    unittest {
+        /// create
+        auto e1 = new HTMLElement("div");
+        assert(equal(e1.tag, "div"));
 
-///
-unittest {
-    auto e1 = new HTMLElement("div");
-    assert(equal(e1.tag, "div"));
+        string[string] attributes;
+        attributes["id"] = "id";
+        attributes["href"] = "#";
 
-    string[string] attributes;
-    attributes["id"] = "id";
-    attributes["href"] = "#";
+        /// with attributes
+        auto e2 = new HTMLElement("a", attributes);
+        assert(equal(e2.attributes.keys, attributes.keys));
+        assert(equal(e2.attributes.values, attributes.values));
 
-    auto e2 = new HTMLElement("a", attributes);
-    assert(equal(e2.attributes.keys, attributes.keys));
-    assert(equal(e2.attributes.values, attributes.values));
+        // with text
+        auto e3 = new HTMLElement("span", "text");
+        assert(equal(e3.text, "text"));
+    }
 }

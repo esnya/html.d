d40ce0f2e1060afe3f799a51b9bdca915a092a12
ukatama <lookout25143@gmail.com>
2014-12-06 02:12:23 +0900

    Add HTMLElement.appendChild

--- html.d
+++ html.d
@@ -14,6 +14,9 @@ class HTMLElement {
     ///
     string text;
 
+    ///
+    HTMLElement[] children;
+
     /// エレメントを生成
     this(string tag, string[string] attributes = null) {
         this.tag = tag;
@@ -43,5 +46,30 @@ class HTMLElement {
     unittest {
         auto span = new HTMLElement("span", "text");
         assert(equal(span.text, "text"));
+    }
+
+    /// 子要素を追加
+    void appendChild(HTMLElement element) {
+        children ~= element;
+    }
+    ///
+    unittest {
+        auto ol = new HTMLElement("ol");
+
+        auto i1 = new HTMLElement("li");
+        auto a1 = new HTMLElement("a", "link1");
+
+        auto i2 = new HTMLElement("li");
+        auto a2 = new HTMLElement("a", "link2");
+
+        ol.appendChild(i1);
+        i1.appendChild(a1);
+
+        i2.appendChild(a2);
+        ol.appendChild(i2);
+
+        assert(equal(ol.children, [i1, i2]));
+        assert(equal(i1.children, [a1]));
+        assert(equal(i2.children, [a2]));
     }
 }
